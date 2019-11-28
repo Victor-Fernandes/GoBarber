0e8dec36973d14608c495ae7891ef01c88a3e388
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-11-28 16:40:07 -0300

    add store and update user, middleware, "

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -16,6 +16,32 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: 'User alredy exist' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
