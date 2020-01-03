9fe5b555a2055ad6c200b2157a36933e78f451b1
Victor Fernandes <victor.fernandes.07@gmail.com>
2020-01-03 15:47:50 -0300

    adicionando e testando upload

---
+++ backend/src/config/multer.js
@@ -0,0 +1,17 @@
+//config para upload de arquivo
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'upload' ),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if(err) return cb(err);
+
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      })
+    },
+  }),
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,23 @@
 import { Router } from 'express';
-
+import multer from 'multer'
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/file', upload.single('file'), (req, res) => {
+  return res.json({ok:true})
+})
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 
 export default routes;
