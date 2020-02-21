16548f9dbfd1952fa4b4a6b1ba838b06d239b690
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-02-21 18:56:20 -0300

    adicionando upload de imgs e listagem de providers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import routes from './routes';
 import './database';
@@ -13,6 +14,12 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+
+    // serve arquivos estaticos q podem ser abertos no browser
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,26 @@
 import { Router } from 'express';
 import multer from 'multer';
-import multerConfig from './config/multer';
+import configMulter from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-const upload = multer(multerConfig);
+const upload = multer(configMulter);
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'aqui funciona' });
-});
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-routes.post('/file', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
