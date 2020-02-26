a49c35001914b4ddecedab40b74fe0159fc1758f
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-02-25 23:53:35 -0300

    verificação se data do agendamento já passou, e se a data está disponivel

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,5 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment';
@@ -28,6 +29,32 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(401).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * check date for availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(401)
+        .json({ error: 'Appointments date is not available!' });
     }
 
     const appointment = await Appointment.create({
