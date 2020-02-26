940be71e6b6c2aec627f90188df4db979417e439
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-02-26 16:08:47 -0300

    create route de listagem de schedule

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import SchedulleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -20,8 +21,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/schedule', SchedulleController.index);
 
 export default routes;
