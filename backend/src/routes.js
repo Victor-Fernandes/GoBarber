3e850790d1df8ed47eedc381005741684813dea5
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-02-28 14:26:12 -0300

    create route delete appointments

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -23,6 +23,7 @@ routes.put('/users', UserController.update);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
