b1646968e0349d31d61bd17d7fa479caa8d5f0fe
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-02-28 17:49:17 -0300

    configurações para envio de email

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '47be1bb817de6c',
+    pass: '41c7dd783008d8',
+  },
+  // default: {
+  //   from: 'Equipe GoBarber <noreply@gobarber.com>',
+  // },
+};
