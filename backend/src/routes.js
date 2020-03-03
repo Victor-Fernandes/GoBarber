769fe377219477a4250ea9a9bc3d43a62821476a
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-03-02 21:10:43 -0300

    add da rota de available

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import SchedulleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -26,6 +27,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/schedule', SchedulleController.index);
