7659d15c763bf2fd5e53ba292a6c1ae87a3d0135
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-06-02 07:02:36 -0300

    usando focus nos inputs

--- appgobarber/src/components/Input/index.tsx
+++ appgobarber/src/components/Input/index.tsx
@@ -1,8 +1,10 @@
 import React, {
   useEffect,
+  useState,
   useRef,
   useImperativeHandle,
   forwardRef,
+  useCallback,
 } from 'react';
 // useImperativeHandle : -> serve para passar uma funcionalidade de um componente inter para um componente pai
 import { TextInputProps } from 'react-native';
@@ -22,16 +24,29 @@ interface InputValueReferencies {
 interface InputRef {
   focus(): void;
 }
+
 const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   { name, icon, ...rest },
   ref,
 ) => {
   const inputElementRef = useRef<any>(null);
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
   // infos para cadastrar o input dentro do unform
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
 
   const inputValueRef = useRef<InputValueReferencies>({ value: defaultValue });
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
 
   useImperativeHandle(ref, () => ({
     focus() {
@@ -58,14 +73,20 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
 
       <TextInput
         ref={inputElementRef}
         // so funciona para IOS keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
