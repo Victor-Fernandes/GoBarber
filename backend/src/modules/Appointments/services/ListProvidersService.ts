f8833501aa9f700dc229fb4e3b468786bb29ba5b
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-08-18 21:42:17 -0300

    correçao na exibiçao dos dados e reset do cache

--- backend/src/modules/Appointments/services/ListProvidersService.ts
+++ backend/src/modules/Appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import User from '@modules/Users/infra/typeorm/entities/User';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepositories from '@modules/Users/repositories/IUsersRepositories';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -29,7 +30,10 @@ class ShowProfileService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users)
+      );
     }
 
     return users;
